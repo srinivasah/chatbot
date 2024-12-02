82d58bd09aae20ef3558fd688878b57b05fe6ef2
srinivasah <srinivasaguptar@gmail.com>
2024-12-02 16:16:57 +0530

    Add files via upload

---
+++ chatbot-master/chatbot-master/constants.js
@@ -0,0 +1,98 @@
+// Options the user could type in
+const prompts = [
+  ["hi", "hey", "hello", "good morning", "good afternoon","Baby" ],
+  ["how are you", "how is life", "how are things"],
+  ["what are you doing", "what is going on", "what is up"],
+  ["how old are you"],
+  ["who are you", "are you human", "are you bot", "are you human or bot"],
+  ["who created you", "who made you"],
+  [
+    "your name please",
+    "your name",
+    "may i know your name",
+    "what is your name",
+    "what call yourself"
+  ],
+  ["i love you"],
+  ["happy", "good", "fun", "wonderful", "fantastic", "cool"],
+  ["bad", "bored", "tired"],
+  ["help me", "tell me story", "tell me joke"],
+  ["ah", "yes", "ok", "okay", "nice"],
+  ["bye", "good bye", "goodbye", "see you later"],
+  ["what should i eat today"],
+  ["bro"],
+  ["what", "why", "how", "where", "when"],
+  ["no","not sure","maybe","no thanks"],
+  [""],
+  ["haha","ha","lol","hehe","funny","joke"],
+  ["how do you play volleyball", "tell me about volleyball", "what is volleyball", "explain volleyball"],
+  ["how many players in valleyball"],
+  ["serve"],
+  ["spike in valleyball"],
+  ["how do you block in volleyball"],
+  ["dig in a volleyball"],
+  ["how do you pass in volleyball"],
+  ["who won the last volleyball world championship"],
+  ["tell me some famous volleyball players"]
+]
+
+// Possible responses, in corresponding order
+
+const replies = [
+  ["Hello!", "Hi!", "Hey!", "Hi there!","Howdy", "i am fine"],
+  [
+    "Fine... how are you?",
+    "Pretty well, how are you?",
+    "Fantastic, how are you?"
+  ],
+  [
+    "Nothing much",
+    "About to go to sleep",
+    "Can you guess?",
+    "I don't know actually"
+  ],
+  ["I am infinite"],
+  ["I am just a bot", "I am a bot. What are you?"],
+  ["The one true God, JavaScript"],
+  ["I am nameless", "I don't have a name"],
+  ["I love you too", "Me too"],
+  ["Have you ever felt bad?", "Glad to hear it"],
+  ["Why?", "Why? You shouldn't!", "Try watching TV"],
+  ["What about?", "Once upon a time..."],
+  ["Tell me a story", "Tell me a joke", "Tell me about yourself"],
+  ["Bye", "Goodbye", "See you later"],
+  ["Sushi", "Pizza"],
+  ["Bro!"],
+  ["Great question"],
+  ["That's ok","I understand","What do you want to talk about?"],
+  ["Please say something :("],
+  ["Haha!","Good one!"],
+  ["Volleyball is a team sport where two teams of six players each try to score points by sending a ball over a net and into the opposing team's court. Players can't hit the ball twice in a row, and the ball must be hit with a clean pass or volley.In volleyball, each team tries to score by getting the ball over the net and onto the opponent's side. Players rotate positions and each team has three hits per side to return the ball."],
+  ["A volleyball team consists of six players on the court at a time: three in the front row and three in the back row.There are six players on each volleyball team during a match. The team rotates positions after winning a serve."],
+  ["A serve is aof putting the ball into The player serves from behind the baseline and must hit the ball over the net into the opponent's court"],
+  ["A spike is a forceful hit where the player attempts to send the ball down into the opponent's court with speed and precision"],
+  ["A block is when a player jumps near the net to intercept a spike from the opposing team. It's a defensive technique used to prevent the ball from crossing the net."],
+  ["A dig is a defensive move used to pass the ball after the opponent's attack. The player typically uses their forearms to dig the ball and direct it to their teammates."],
+  ["In volleyball, a pass is when a player receives the ball and sends it to a teammate. The most common type of pass is the bump pass, where players use their forearms to guide the ball."],
+  ["The last Volleyball World Championship was won by Italy in 2022, beating Brazil in the final to claim their fourth title."],
+  ["Some of the best volleyball players include Karch Kiraly (USA), Giba (Brazil), and Sergey Tetyukhin (Russia). These players have made a huge impact in international volleyball."]
+]
+
+
+// Random for any other user input
+
+const alternative = [
+  "Same",
+  "Go on...",
+  "Bro...",
+  "Try again",
+  "I'm listening...",
+  "I don't understand :/"
+]
+
+// Whatever else you want :)
+
+const coronavirus = ["Please stay home", "Wear a mask", "Fortunately, I don't have COVID", "These are uncertain times"]
+
+
+
---
+++ chatbot-master/constants.js
@@ -0,0 +1,102 @@
+// Prompts (user inputs)
+const prompts = [
+  ["hi", "hey", "hello", "good morning", "good afternoon", "hj"],
+  ["how are you", "how is life", "how are things"],
+  ["what are you doing", "what is going on", "what is up"],
+  ["how old are you"],
+  ["who are you", "are you human", "are you bot", "are you human or bot"],
+  ["who created you", "who made you"],
+  ["your name please", "your name", "may i know your name", "what is your name", "what do you call yourself"],
+  ["i love you"],
+  ["happy", "good", "fun", "wonderful", "fantastic", "cool"],
+  ["bad", "bored", "tired"],
+  ["help me", "tell me story", "tell me joke"],
+  ["ah", "yes", "ok", "okay", "nice"],
+  ["bye", "good bye", "goodbye", "see you later"],
+  ["what should i eat today"],
+  ["bro"],
+  ["what", "why", "how", "where", "when"],
+  ["no", "not sure", "maybe", "no thanks"],
+  [""],
+  ["haha", "ha", "lol", "hehe", "funny", "joke"],
+  
+  // Volleyball-specific prompts
+  ["how do you play volleyball", "tell me about volleyball", "what is volleyball", "explain volleyball"],
+  ["how many players are on a volleyball team", "how many players in volleyball"],
+  ["what is a serve", "what is a volleyball serve"],
+  ["what's a spike in volleyball", "how do you spike a volleyball"],
+  ["what's a block in volleyball", "how do you block in volleyball"],
+  ["what's a dig in volleyball", "how do you dig a volleyball"],
+  ["how do you pass in volleyball", "what's a pass in volleyball"],
+  ["who won the last volleyball world championship", "who is the best volleyball team"],
+  ["tell me some famous volleyball players", "who are the top volleyball players"]
+];
+
+// Replies (responses)
+const replies = [
+  ["hello", "Hi!", "Hey!", "Hi there!", "Howdy", "I am fine"],
+  [
+    "Fine... how are you too?",
+    "Pretty well, how are you?",
+    "Fantastic, how are you?"
+  ],
+  [
+    "Nothing much",
+    "About to go to sleep",
+    "Can you guess?",
+    "I don't know actually"
+  ],
+  ["I am infinite"],
+  ["I am just a bot", "I am a bot. What are you?"],
+  ["The one true God, JavaScript"],
+  ["I am nameless", "I don't have a name"],
+  ["I love you too", "Me too"],
+  ["Have you ever felt bad?", "Glad to hear it"],
+  ["Why?", "Why? You shouldn't!", "Try watching TV"],
+  ["What about?", "Once upon a time..."],
+  ["Tell me a story", "Tell me a joke", "Tell me about yourself"],
+  ["Bye", "Goodbye", "See you later"],
+  ["Sushi", "Pizza"],
+  ["Bro!"],
+  ["Great question"],
+  ["That's ok", "I understand", "What do you want to talk about?"],
+  ["Please say something :("],
+  ["Haha!", "Good one!"],
+
+  // Volleyball-specific responses
+  [
+    "Volleyball is a team sport where two teams of six players each try to score points by sending a ball over a net and into the opposing team's court. Players can't hit the ball twice in a row, and the ball must be hit with a clean pass or volley.",
+    "In volleyball, each team tries to score by getting the ball over the net and onto the opponent's side. Players rotate positions and each team has three hits per side to return the ball."
+  ],
+  [
+    "A volleyball team consists of six players on the court at a time: three in the front row and three in the back row.",
+    "There are six players on each volleyball team during a match. The team rotates positions after winning a serve."
+  ],
+  [
+    "A serve is the action of putting the ball into play. The player serves from behind the baseline and must hit the ball over the net into the opponent's court. A serve can be either underhand or overhand.",
+    "A volleyball serve is the first action in the game, where one player serves the ball over the net to the opposing team. There are different types of serves like float serves and jump serves."
+  ],
+  [
+    "A spike is a forceful hit where the player attempts to send the ball down into the opponent's court with speed and precision. It’s usually performed after a set and can be a game-winning move.",
+    "To spike a volleyball, you need to jump high and hit the ball with the palm of your hand, aiming to send it down on the opponent's side of the court."
+  ],
+  [
+    "A block is when a player jumps near the net to intercept a spike from the opposing team. It's a defensive technique used to prevent the ball from crossing the net.",
+    "In volleyball, a block is used to stop the opponent's attack. A player jumps near the net to try and deflect or stop the ball coming from a spike."
+  ],
+  [
+    "A dig is a defensive move used to pass the ball after the opponent's attack. The player typically uses their forearms to ‘dig’ the ball and direct it to their teammates.",
+    "A dig is a volleyball move where a player uses their forearms or hands to pass or control the ball after the opponent's attack, typically from a spike."
+  ],
+  [
+    "In volleyball, a pass is when a player receives the ball and sends it to a teammate. The most common type of pass is the bump pass, where players use their forearms to guide the ball.",
+    "To pass a volleyball, you typically use your forearms or hands to send the ball to your teammate. Passing is a fundamental skill in volleyball."
+  ],
+  [
+    "The last Volleyball World Championship was won by Italy in 2022, beating Brazil in the final to claim their fourth title.",
+    "Brazil is a dominant team in volleyball, but teams like Italy, Poland, and the United States have also been competitive in recent years."
+  ],
+  [
+    "Some of the best volleyball players include Karch Kiraly (USA), Giba (Brazil), and Sergey Tetyukhin (Russia). These players have made a huge impact in international volleyball."
+  ]
+];
